0b0883025a14963a951ab857ee3ca8b1df66a0c1
Kamol Chandra Roy <kamol.roy@optimizely.com>
2024-08-03 00:47:35 +0600

    Added view all jobs component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListing from './components/JobListings'
+import ViewAllJobs  from './components/ViewAllJobs'
 
 
 const App = () => {
@@ -11,18 +12,8 @@ const App = () => {
       <Navbar/>
       <Hero/>  
       <HomeCards/>
-      <JobListing/>
-     
-
-     
-
-  
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a>
-      </section>
+      <JobListing/>    
+      <ViewAllJobs/>     
     </>
   )
 }
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -4,6 +4,8 @@ import JobListing from './JobListing';
 
 const JobListings = () => {
 
+  const recentJobs = Jobs.slice(0,3);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -11,7 +13,7 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {Jobs.map((job) => (
+          {recentJobs.map((job) => (
             <JobListing key={job.id} job={job}/>
           ))}
         </div>
