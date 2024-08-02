48cd5565d3fbf9ecc1b5b6e08661360cc5d0522a
Kamol Chandra Roy <kamol.roy@optimizely.com>
2024-08-02 22:50:35 +0600

    Get git of the dummy code and practice the basic things of react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+    const name = 'kamol';
+    const x = 55;
+    const y = 45;
+    const names = ['kamol', "marry", 'joe', 'sara'];
+    const loggedIn = true;
+    const styles = {
+        color: 'red',
+        fontSize: '55px'
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // if(loggedIn){
+    //     return <h1>Hello Member</h1>
+    // }
+
+    return (
+        <>
+            <div className='text-5xl'>App</div>
+            <p style={styles}> Hello {name}</p>
+            <p>The sum of {x} and {y} is {x + y}</p>
+            <ul>
+                {names.map((name, index) => (
+                    <li key={index}>{name}</li>
+                ))}
+            </ul>
+
+            {loggedIn && <h1>Hello Members</h1>}
+        </>
+    )
 }
 
 export default App
