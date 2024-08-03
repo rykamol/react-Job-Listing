8455f11d3cb60609f419ef127f6a6dd37ef60981
Kamol Chandra Roy <kamol.roy@optimizely.com>
2024-08-03 15:06:52 +0600

    Complete jobs page long with accurate job number and title

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Joblistings from '../components/JobListings'
 
 const JobsPage = () => {
   return (
-    <div>JobsPage</div>
+      <section className='bg-blue-50 px-4 py-6'>
+        <Joblistings isHome={false}/>
+      </section>
   )
 }
 
