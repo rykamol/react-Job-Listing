109dd169ec897c1cf54e23735ae3f6e0236f08f8
Kamol Chandra Roy <kamol.roy@optimizely.com>
2024-08-03 14:02:54 +0600

    Added router and added a home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListing from './components/JobListings'
-import ViewAllJobs  from './components/ViewAllJobs'
+import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage/>}/>)
+)
 const App = () => {
-  return (
-    <>
-      <Navbar/>
-      <Hero/>  
-      <HomeCards/>
-      <JobListing/>    
-      <ViewAllJobs/>     
-    </>
-  )
+  return    <RouterProvider router={router}/>
 }
 
 export default App
