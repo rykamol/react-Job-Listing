dfbf108e099e82a4484a53f7f0256339a5b29d3a
Kamol Chandra Roy <kamol.roy@optimizely.com>
2024-08-04 11:59:52 +0600

    Load single job using data loader

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,35 +1,20 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useLoaderData } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 const JobPage = () => {
-  const {id} = useParams();
-  const [job, setJob] = useState(null);
-  const [loading,setLoading]=useState(true);
-
-  useEffect(() => {
-
-    const fetchJobs = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`)
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.log('Error fetching job', error)
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchJobs();
-  }, [])
-
+  const { id } = useParams();
+  const job = useLoaderData();
 
   return (
-    loading?<Spinner/>:(
-      <h1>{job.title}</h1>
-    )
+    <h1>Loding data</h1>
   )
 }
 
-export default JobPage
+
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`)
+  const data = await res.json();
+  return data;
+}
+export { JobPage as default, jobLoader }
