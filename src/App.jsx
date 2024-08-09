9e599ca24b1fbfb3e3914b3441b1a01fd7825975
Kamol Chandra Roy <kamol.roy@optimizely.com>
2024-08-10 00:39:39 +0600

    Implement update jobs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, { jobLoader } from './pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 
 
@@ -33,6 +34,20 @@ const App = () => {
     return;
   }
 
+  //Update job
+
+  const updateJob = async (job) => {
+    await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
@@ -41,6 +56,8 @@ const App = () => {
         <Route path='/jobs' element={<JobsPage />} />
         <Route path='/job/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
         <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
+
         <Route path='*' element={<NotFoundPage />} />
 
 
